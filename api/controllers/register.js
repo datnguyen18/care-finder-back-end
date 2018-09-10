05cd1ace94400a24ed56a9cfd612ac6fd8697e33
Unknown <dat.nguyentien.set13@eiu.edu.vn>
2018-09-10 21:29:58 +0700

    update

---
+++ api/controllers/register.js
@@ -0,0 +1,91 @@
+const mongoose = require('mongoose');
+const validateRegisterInput = require('../validation/register');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+const Doctor = require('../models/doctor');
+
+exports.register_user = (req, res) => {
+    const {
+        errors,
+        isValid
+    } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+            return res.status(500).json({
+                error: err
+            })
+        } else {
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: hash,
+                gender: req.body.gender
+            })
+            user.save()
+                .then(result => {
+                    console.log(result);
+                    res.status(200).json({
+                        message: 'Handling POST requests to /register ',
+                        createdUser: user
+                    })
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        error: err
+                    })
+                })
+        }
+    })
+}
+
+exports.register_doctor = (req,res) => {
+    console.log('====================================');
+    console.log(req.files);
+    console.log('====================================');
+    const {
+        errors,
+        isValid
+    } = validateRegisterInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+            return res.status(500).json({
+                error: err
+            })
+        } else {
+            const doctor = new Doctor({
+                _id: new mongoose.Types.ObjectId(),
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: hash,
+                gender: req.body.gender,
+                imageOfIdentification: "http://localhost:3000/uploads/"+req.files.imageOfIdentification[0].filename,
+                imageOfDiploma: "http://localhost:3000/uploads/"+req.files.imageOfDiploma[0].filename
+            })
+            doctor.save()
+                .then(result => {
+                    console.log(result);
+                    res.status(200).json({
+                        message: 'Handling POST requests to /register ',
+                        createdUser: doctor
+                    })
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        error: err
+                    })
+                })
+        }
+    })
+}
--- api/route/register.js
+++ api/route/register.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const validateRegisterInput = require('../validation/register');
-const bcrypt = require('bcrypt');
+
 const multer = require('multer');
+const RegisterController = require('../controllers/register');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -16,93 +15,10 @@ const storage = multer.diskStorage({
   
 const upload = multer({ storage: storage })
 
-const User = require('../models/user');
-const Doctor = require('../models/doctor');
 
-router.post('/', (req, res) => {
-    const {
-        errors,
-        isValid
-    } = validateRegisterInput(req.body);
 
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({
-                error: err
-            })
-        } else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: hash,
-                gender: req.body.gender
-            })
-            user.save()
-                .then(result => {
-                    console.log(result);
-                    res.status(200).json({
-                        message: 'Handling POST requests to /register ',
-                        createdUser: user
-                    })
-                })
-                .catch(err => {
-                    res.status(500).json({
-                        error: err
-                    })
-                })
-        }
-    })
+router.post('/', RegisterController.register_user)
 
-})
-
-router.post('/doctor', upload.fields([{name: 'imageOfIdentification'}, {name:'imageOfDiploma'}]), (req,res) => {
-    console.log('====================================');
-    console.log(req.files);
-    console.log('====================================');
-    const {
-        errors,
-        isValid
-    } = validateRegisterInput(req.body);
-
-    if (!isValid) {
-        return res.status(400).json(errors);
-    }
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if (err) {
-            return res.status(500).json({
-                error: err
-            })
-        } else {
-            const doctor = new Doctor({
-                _id: new mongoose.Types.ObjectId(),
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: hash,
-                gender: req.body.gender,
-                imageOfIdentification: "http://localhost:3000/uploads/"+req.files.imageOfIdentification[0].filename,
-                imageOfDiploma: "http://localhost:3000/uploads/"+req.files.imageOfDiploma[0].filename
-            })
-            doctor.save()
-                .then(result => {
-                    console.log(result);
-                    res.status(200).json({
-                        message: 'Handling POST requests to /register ',
-                        createdUser: doctor
-                    })
-                })
-                .catch(err => {
-                    res.status(500).json({
-                        error: err
-                    })
-                })
-        }
-    })
-})
+router.post('/doctor', upload.fields([{name: 'imageOfIdentification'}, {name:'imageOfDiploma'}]), RegisterController.register_doctor)
 
 module.exports = router

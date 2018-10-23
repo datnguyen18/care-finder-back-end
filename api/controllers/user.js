204c0d9fcdf44f884ca24cca0f78276954fd0275
datnguyen <dat.nguyentien.set13@eiu.edu.vn>
2018-10-23 11:43:46 +0700

    fix controller get current user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,6 +4,16 @@ exports.get_current_user = (req,res) => {
   const userId = req.userData.userId;
 
   User.findById(userId).exec()
-    .then(doc => res.status(200).json({doc}))
+    .then(doc =>{
+      const {lastName, firstName, gender,phoneNumber, follows} = doc;
+
+      res.status(200).json({
+        lastName,
+        firstName,
+        phoneNumber,
+        follows,
+        gender
+      })
+    })
     .catch(err => res.status(404).json({err}))
 }

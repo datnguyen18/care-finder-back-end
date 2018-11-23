69f9396d1293532ca156fc9a7ae3652e6629f7bf
datnguyen <dat.nguyentien.set13@eiu.edu.vn>
2018-11-23 10:59:29 +0700

    add route for doctor

--- api/route/doctor.js
+++ api/route/doctor.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const doctorController = require('../controllers/doctor');
-//get all doctors
-router.get('/', doctorController.get_all_doctors );
 
 //get unauthenticated doctors
 router.get('/unauthenticated', doctorController.get_unauthenticated_doctors);
@@ -14,15 +12,17 @@ router.get('/authenticated', doctorController.get_authenticated_doctors);
 //authenticate doctor
 router.patch('/verify/:idDoctor',(req,res) => {
     const id = req.params.idDoctor;
-    Doctor.findOneAndUpdate({_id: id},{$set: {isVerified: req.body.isVerified}},{new: true})
-        .exec()
-        .then(result => {
-            if(!result){return res.status(500).json({error:"Id not found"})}
-            console.log(result);
-            res.status(200).json(result)
-        })
-        .catch(err => {
-            res.status(500).json({error: err})
-        });
+    Doctor.findOneAndUpdate({_id: id},{$set: {permission: 'DOCTOR'}},{new: true})
+      .exec()
+      .then(result => {
+          if(!result){return res.status(500).json({error:"Id not found"})}
+          console.log(result);
+          res.status(200).json(result)
+      })
+      .catch(err => {
+          res.status(500).json({error: err})
+      });
 })
+
+router.patch('/authorization', doctorController.authourize_user);
 module.exports = router;

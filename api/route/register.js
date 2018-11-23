7305ca4dab3fb0a0d3a5e3184f6d005e3214df8d
datnguyen <dat.nguyentien.set13@eiu.edu.vn>
2018-11-23 10:59:43 +0700

    delete route

--- api/route/register.js
+++ api/route/register.js
@@ -6,6 +6,6 @@ const upload = require('../config/multer');
 
 router.post('/', RegisterController.register_user);
 router.post('/verify', RegisterController.verify_phone_number);
-router.post('/doctor', upload.fields([{ name: 'imageOfIdentification' }, { name: 'imageOfDiploma' }]), RegisterController.register_doctor)
+// router.post('/doctor', upload.fields([{ name: 'imageOfIdentification' }, { name: 'imageOfDiploma' }]), RegisterController.register_doctor)
 
 module.exports = router

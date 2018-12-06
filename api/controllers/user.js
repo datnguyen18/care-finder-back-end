f28228660925b9391ed10a9cc4fb7d0ede45b34b
datnguyen18 <dat.nguyentien.set13@eiu.edu.vn>
2018-12-06 10:46:03 +0700

    add user change password

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 exports.get_user = (req, res) => {
   const userId = req.params.idUser;
@@ -72,5 +73,53 @@ exports.require_be_doctor = (req, res) => {
         err
       })
   })
+}
 
+exports.update_information_user = (req, res) => {
+  User.findByIdAndUpdate(req.params.idUser, {$set: req.body}, {new: true}).exec()
+    .then(doc => {
+      res.status(200).json({
+        doc
+      })
+    })
+    .catch(err => {
+      res.status(404).json({
+        err
+      })
+    })
+}
+
+exports.change_password = async (req, res) => {
+  const {oldPassword, newPassword} = req.body;
+  User.findById(req.params.idUser).exec()
+    .then(user => {
+      bcrypt.compare(oldPassword, user.password, async (err, result) => {
+        if(!result){
+          return res.status(400).json({
+            result: "Mật khẩu cũ sai"
+          })
+        }
+
+        if(result) {
+          const hashNewPassword = await bcrypt.hash(newPassword, 10);
+          User.findByIdAndUpdate(req.params.idUser, {$set: {password: hashNewPassword}}).exec()
+            .then(doc => {
+              return res.status(200).json({
+                result: "Change password successfully"
+              })
+            })
+            .catch(error => {
+              res.status(404).json({
+                err
+              })
+            })
+          
+        }
+      })
+    })
+    .catch(err => {
+      res.status(404).json({
+        err
+      })
+    })
 }
--- api/route/user.js
+++ api/route/user.js
@@ -5,7 +5,9 @@ const checkAuth = require('../middleware/check-auth');
 const upload = require('../config/multer');
 
 router.get('/me', checkAuth, UserController.get_current_user);
-router.patch('/:idUser', checkAuth, upload.fields([{ name: 'imageOfIdentificationFront' },{ name: 'imageOfIdentificationBack' }, { name: 'imageOfDiploma' }]), UserController.require_be_doctor);
+router.patch('/verify/:idUser', checkAuth, upload.fields([{ name: 'imageOfIdentificationFront' },{ name: 'imageOfIdentificationBack' }, { name: 'imageOfDiploma' }]), UserController.require_be_doctor);
 router.patch('/', checkAuth, upload.single('avatar'), UserController.update_user);
+router.patch('/:idUser', checkAuth, UserController.update_information_user);
 router.get('/:idUser', UserController.get_user);
+router.patch('/password/:idUser', checkAuth, UserController.change_password)
 module.exports = router;

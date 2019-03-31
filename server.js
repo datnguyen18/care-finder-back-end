a2fa501e4384dd7ddc225c8d594d67fac4624663
datnguyen18 <dat.nguyentien.set13@eiu.edu.vn>
2019-03-31 21:56:34 +0700

    fix server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const app = require('./app')
 const port = process.env.PORT || 3000;
-
+const socketio = require('socket.io');
 const server = http.createServer(app);
+const websocket = socketio(server);
 
 server.listen(port, () => {
     console.log(`🚀 Started on port ${port}`);
+    websocket.on('connect', (socket) => {
+      console.log('A person just conected', socket.id)
+    })
 });
